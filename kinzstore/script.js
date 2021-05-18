69ac8ceaa6fc6b552358b5fe85074564f42890e2
kayleighhansen <kayleigh@hansens.org>
2021-05-18 17:57:53 -0600

    ...

--- kinzstore/script.js
+++ kinzstore/script.js
@@ -1,25 +1,37 @@
 const totalInventory = document.querySelector('.total-inventory');
 
 function renderInventory(doc){
-    totalInventory.innerHTML += `<div class="inventory-item">`;
     const inventoryItem = document.querySelector('.inventory-item');
 
     var li = document.createElement('h4');
     var price = document.createElement('p');
 
-    li.setAttribute('data-id', doc.id);
+    price.setAttribute('class', 'price');
+    inventoryItem.setAttribute('data-id', doc.id);
     li.textContent = doc.data().name;
-    price.textContent = doc.data().price;
 
+    price.textContent = `$`;
+    price.textContent += doc.data().price;
+    
     inventoryItem.append(li);
     inventoryItem.append(price);
 
     inventoryItem.innerHTML += `</div>`;
-
 }
 
 db.collection('clothes').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
+        totalInventory.innerHTML += `<div class="inventory-item">`;
         renderInventory(doc);
     })
 });
+
+var sec = 0;
+    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    setInterval( function(){
+        document.getElementById("seconds").innerHTML=pad(++sec%60);
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    }, 1000);
+var seconds = document.querySelector('#seconds');
+
+var wallet = document.querySelector(".employee-wallet");
