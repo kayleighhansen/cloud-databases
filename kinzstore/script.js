bf80c19f4c7ddd0dc51e0aaf7afe0cf7e83315f4
kayleighhansen <kayleigh@hansens.org>
2021-05-18 11:48:51 -0600

    items are loading

--- kinzstore/script.js
+++ kinzstore/script.js
@@ -1,3 +1,25 @@
+const totalInventory = document.querySelector('.total-inventory');
+
+function renderInventory(doc){
+    totalInventory.innerHTML += `<div class="inventory-item">`;
+    const inventoryItem = document.querySelector('.inventory-item');
+
+    var li = document.createElement('h4');
+    var price = document.createElement('p');
+
+    li.setAttribute('data-id', doc.id);
+    li.textContent = doc.data().name;
+    price.textContent = doc.data().price;
+
+    inventoryItem.append(li);
+    inventoryItem.append(price);
+
+    inventoryItem.innerHTML += `</div>`;
+
+}
+
 db.collection('clothes').get().then((snapshot) => {
-    console.log(snapshot.docs);
+    snapshot.docs.forEach(doc => {
+        renderInventory(doc);
+    })
 });
