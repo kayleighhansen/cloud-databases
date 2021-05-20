09f107ecf98271c6dbd8277eac87add2bbee43d9
kayleighhansen <kayleigh@hansens.org>
2021-05-20 02:08:38 -0600

    got the things adding

--- kinzstore/script.js
+++ kinzstore/script.js
@@ -1,13 +1,22 @@
 const totalInventory = document.querySelector('.total-inventory');
 
 function renderInventory(doc){
-    const inventoryItem = document.querySelector('.inventory-item');
+    totalInventory.innerHTML += `<div class="inventory-item" id="${doc.id}">`;
+    const inventoryItem = document.querySelector('#' + doc.id);
 
     var li = document.createElement('h4');
     var price = document.createElement('p');
+    var deleteButton = document.createElement('button');
+    var detailsButton = document.createElement('button');
 
     price.setAttribute('class', 'price');
-    inventoryItem.setAttribute('data-id', doc.id);
+    deleteButton.setAttribute('class', 'delete');
+    detailsButton.setAttribute('class', 'details');
+
+    deleteButton.innerHTML = "X";
+    detailsButton.innerHTML = "See More";
+
+    inventoryItem.setAttribute('id', doc.id);
     li.textContent = doc.data().name;
 
     price.textContent = `$`;
@@ -15,16 +24,50 @@ function renderInventory(doc){
     
     inventoryItem.append(li);
     inventoryItem.append(price);
+    inventoryItem.append(detailsButton);
+    inventoryItem.append(deleteButton);
+
 
     inventoryItem.innerHTML += `</div>`;
 }
 
 db.collection('clothes').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
-        totalInventory.innerHTML += `<div class="inventory-item">`;
         renderInventory(doc);
     })
 });
+
+const addNew = document.querySelector(".addNew").addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    var newName = document.querySelector(".newName").value;
+    console.log(newName);
+    var newPrice = document.querySelector(".newPrice").value;
+    console.log(newPrice);
+
+    db.collection("clothes").add({
+        name: newName,
+        price: newPrice 
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+
+});
+
+
+const buttons = document.querySelectorAll(".delete");
+console.log(buttons);
+
+// for (var button of buttons) {
+//     button.addEventListener('click', function(event) {
+//         console.log("delete");
+//     })
+// };
 
 var sec = 0;
     function pad ( val ) { return val > 9 ? val : "0" + val; }
@@ -33,5 +76,3 @@ var sec = 0;
         document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
     }, 1000);
 var seconds = document.querySelector('#seconds');
-
-var wallet = document.querySelector(".employee-wallet");
