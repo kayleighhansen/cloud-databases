a6aec8c1c6e0762d6ff64966b0f2870ef6baff9f
kayleighhansen <kayleigh@hansens.org>
2021-05-22 16:44:23 -0600

    saivng firebase data

---
+++ kinzstore/firebase.js
@@ -0,0 +1,16 @@
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDf9H5OfSjz7MAPlytOJQNojN65xzsFQ9M",
+//     authDomain: "kinzstore-51c68.firebaseapp.com",
+//     projectId: "kinzstore-51c68",
+//     storageBucket: "kinzstore-51c68.appspot.com",
+//     messagingSenderId: "1030560301653",
+//     appId: "1:1030560301653:web:ef4260e351e404d955cadc",
+//     measurementId: "G-YX0TWNXTNV"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+// const db = firebase.firestore();
+// db.settings({ timestampsInSnapshots: true});
--- kinzstore/script.js
+++ kinzstore/script.js
@@ -1,6 +1,23 @@
 // select the inventory div, this is where all the magic is going to happen
 const totalInventory = document.querySelector('.total-inventory');
 
+var firebaseConfig = {
+    apiKey: "AIzaSyDf9H5OfSjz7MAPlytOJQNojN65xzsFQ9M",
+    authDomain: "kinzstore-51c68.firebaseapp.com",
+    projectId: "kinzstore-51c68",
+    storageBucket: "kinzstore-51c68.appspot.com",
+    messagingSenderId: "1030560301653",
+    appId: "1:1030560301653:web:ef4260e351e404d955cadc",
+    measurementId: "G-YX0TWNXTNV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true});
+
+
 // renders all the inventory when called 
 function renderInventory(doc){
     totalInventory.innerHTML += `<div class="inventory-item" id="K${doc.id}">`;
